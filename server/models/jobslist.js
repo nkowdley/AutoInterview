4046e3d0c9941a022efd11290e097269a5751273
Neel Kowdley <nkowdley@gmail.com>
2015-11-07 08:52:40 -0500

    added Ability to enter jobs into the db

--- server/models/jobslist.js
+++ server/models/jobslist.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 //var app = express();
 var schema = new mongoose.Schema({
   //schema setup
-  
+  jobname: String,
+  company: String,
+  jobdetails: String
 });
 
 module.exports = mongoose.model('jobslist', schema);
--- server/routes/acceptJobs.js
+++ server/routes/acceptJobs.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
+//mongo
 var mongoose = require('mongoose');
+var db=mongoose.connection;
+var joblist= require('../models/jobslist.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* Post home page. */
+router.post('/', function(req, res, next) {
 
+var job=new joblist(
+{ jobname: req.body.jobname,
+  company: req.body.company,
+  jobdetails: req.body.jobdetails
+});
+//save the job to mongo
+job.save(function (err, job) {
+  if (err) return console.error(err);
+});
+
+res.send("Job Accepted!");
 });
 
 module.exports = router;
