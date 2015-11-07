b9e5a2d4eadb664e4ed07c7ce3160ab13519fea6
Nicole Daniels <nzoner12@yahoo.com>
2015-11-07 11:29:09 -0500

    Configure user authentification

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,30 +2,10 @@
 (function() {
 
 function MainController($scope, $http, Upload) {
-  /*
+
   var self = this;
-  this.awesomeThings = [];
-
-  $http.get('/api/things').then(function(response) {
-    self.awesomeThings = response.data;
-  });
-
-  this.addThing = function() {
-    if (self.newThing === '') {
-      return;
-    }
-    $http.post('/api/things', { name: self.newThing });
-    self.newThing = '';
-  };
-
-  this.deleteThing = function(thing) {
-    $http.delete('/api/things/' + thing._id);
-  };
-  */
-  var self = this;
-console.log('poop');
+ 
   this.upload = function (file) {
-    console.log("Gooble gork");
         Upload.upload({
             url: '/acceptResume',
             headers: {'Content-Type': 'multipart/form-data'},
--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,5 +8,19 @@ angular.module('autoInterviewApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
-      });
+      })
+      .state('login', {
+    	url: '/login',
+   		templateUrl: 'app/partials/login.html',
+    	data: {
+      		login: true
+    	}
+  	  })
+      .state('signup', {
+    	url: '/signup',
+   		templateUrl: 'app/partials/signup.html',
+    	data: {
+      		public: true
+    	}
+  	  });
   });
