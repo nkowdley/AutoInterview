f684acb32c74eeddd56360bf42bcc3e516af96a3
Neel Kowdley <nkowdley@gmail.com>
2015-11-06 22:21:08 -0500

    getJobs is done

--- server/routes/getJobs.js
+++ server/routes/getJobs.js
@@ -3,8 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("yolo");
-  res.sendfile('../client/app/index.html');
+
+  db.collection('jobslist').find().toArray(function(err, jobs){
+    if (err)
+    {
+      console.log("Error:(err)");
+      return next(err);
+    }
+    res.send(jobs);
+  });
+
 });
 
 module.exports = router;
