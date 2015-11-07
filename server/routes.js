58f30ca32b133633eda99be80575ae673d62ddf6
Neel Kowdley <nkowdley@gmail.com>
2015-11-06 21:56:01 -0500

    Created initial server framework

--- server/routes.js
+++ server/routes.js
@@ -11,6 +11,9 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
+  app.use('/acceptResume', require('./views/acceptResume')); //acceptResume Sent to us
+  app.use('/getJobs', require('./views/getJobs')); //get a list of all Jobs we have in the db
+  app.use('/acceptJobs', require('./views/acceptJobs')); //allow employers to add jobss
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
