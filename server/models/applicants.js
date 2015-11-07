5307d55ee4fe01a0d00954f190c00c233b3a7e48
Neel Kowdley <nkowdley@gmail.com>
2015-11-07 01:07:23 -0500

    commit all the things(Hail to the Redskins)

--- server/models/applicants.js
+++ server/models/applicants.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 //var app = express();
 var schema = new mongoose.Schema({
   //schema setup
+  name: String,
+  resume: String
 });
 
 module.exports = mongoose.model('applicants', schema);
--- server/routes/acceptResume.js
+++ server/routes/acceptResume.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var multer= require('multer');
 //mongo stuffs
 var mongoose = require('mongoose');
 var db=mongoose.connection;
 var applicant= require('../models/applicants.js');
 /* Accept Post Request page. */
-router.post('/', function(req, res, next) {
+router.post('/',[ multer({ dest: './uploads/'}), function(req, res, next) {
 //create the person object from the post request
 var person=new applicant(
 {name: req.body.name,
-song_name: req.body.resume
+resume: "1"
+});
+//save the items in the db
+person.save(function (err, person) {
+  if (err) return console.error(err);
 });
 
-});
+res.send(req.body);
+}]);
 
 module.exports = router;
